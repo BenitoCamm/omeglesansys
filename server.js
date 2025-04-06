052e3899bcdf0cbf1a151beed5b8fdee11647f9e
MaxRex <157999599+BenitoCamm@users.noreply.github.com>
2025-04-06 13:06:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,67 +1,58 @@
+
+const express = require("express");
+const http = require("http");
 const { Server } = require("socket.io");
 
-const io = new Server({
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-let availablePartners = [];
+app.use(express.static("public"));
+
+let queue = [];
+let rooms = new Map();
 
 io.on("connection", (socket) => {
-  console.log('A user connected: ' + socket.id);
+  console.log("New user connected: ", socket.id);
 
-  socket.on('message', (message) => {
-    io.emit('message', message, socket.id);
-  });
+  queue.push(socket);
 
-  socket.on('findPartner', () => {
-    if (availablePartners.length > 0) {
-      // Pair the current socket with the first available partner
-      const partnerSocketId = availablePartners.shift();
-      const partnerSocket = io.sockets.sockets.get(partnerSocketId);
+  if (queue.length >= 2) {
+    const user1 = queue.shift();
+    const user2 = queue.shift();
+    const roomId = `${user1.id}#${user2.id}`;
 
-      if (partnerSocket) {
-        socket.emit('partnerFound');
-        partnerSocket.emit('partnerFound');
-        console.log(`Matched ${socket.id} with ${partnerSocketId}`);
-      } else {
-        // The partner is no longer available
-        socket.emit('noPartnersAvailable');
-      }
-    } else {
-      // Add the current socket to the list of available partners
-      availablePartners.push(socket.id);
-      socket.emit('noPartnersAvailable');
-      console.log(`Added ${socket.id} to available partners`);
+    user1.join(roomId);
+    user2.join(roomId);
+
+    rooms.set(user1.id, roomId);
+    rooms.set(user2.id, roomId);
+
+    io.to(roomId).emit("chat_start");
+  }
+
+  socket.on("message", (msg) => {
+    const roomId = rooms.get(socket.id);
+    if (roomId) {
+      socket.to(roomId).emit("message", msg);
     }
   });
 
-  socket.on('offer', (offer) => {
-    socket.broadcast.emit('offer', offer);
-  });
-
-  socket.on('answer', (answer) => {
-    socket.broadcast.emit('answer', answer);
-  });
-
-  socket.on('iceCandidate', (candidate) => {
-    socket.broadcast.emit('iceCandidate', candidate);
-  });
-
-  socket.on('startCall', () => {
-    socket.broadcast.emit('startCall');
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected: ' + socket.id);
-
-    // Remove the socket from availablePartners if it exists there
-    availablePartners = availablePartners.filter(partnerId => partnerId !== socket.id);
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
+    const roomId = rooms.get(socket.id);
+    if (roomId) {
+      socket.to(roomId).emit("partner_left");
+      const [id1, id2] = roomId.split("#");
+      rooms.delete(id1);
+      rooms.delete(id2);
+    } else {
+      queue = queue.filter((s) => s.id !== socket.id);
+    }
   });
 });
 
-io.listen(3000);
-console.log('Server listening on port 3000');
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
 
